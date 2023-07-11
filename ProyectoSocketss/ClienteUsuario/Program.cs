cb7be94bec9091195a936d8d347cc7bed275d31d
migecvr <101593533+migecvr@users.noreply.github.com>
2023-07-10 20:56:13 -0400

    Add files via upload

---
+++ ProyectoSocketss/ClienteUsuario/Program.cs
@@ -0,0 +1,138 @@
+﻿using ClienteSocketUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteUsuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool repetir = true;
+            do
+            {
+                repetir = true;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("-------------------------------------------------------------------------------");
+                Console.WriteLine("Ingresa el numero del puerto y direccion ip del servidor al cual vas a conectar");
+                Console.WriteLine("ejemplo");
+                Console.WriteLine("Puerto:2050");
+                Console.WriteLine("IP:127.0.0.1");
+                Console.WriteLine("-------------------------------------------------------------------------------");
+
+
+                int puerto;
+                bool esValido = true;
+
+                do
+                {
+                    esValido = true;
+                    Console.Write("Puerto: ");
+                    esValido = Int32.TryParse(Console.ReadLine().Trim(), out puerto);
+                    if (esValido && (puerto >= 0 && puerto <= 65535))
+                    {
+                        esValido = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero de puerto ingresado no es valido");
+                        esValido = false;
+                    }
+                } while (!esValido);
+                
+
+                
+                
+                
+                
+                Console.Write("Servidor: ");
+                string servidor = Console.ReadLine().Trim();
+
+                Console.WriteLine("Conectando a Servidor {0} en puerto {1}", servidor, puerto);
+                ClienteSocket clienteSocket = new ClienteSocket(servidor, puerto);
+                if (clienteSocket.Conectar() && esValido == true)
+                {
+                    Console.WriteLine("Conectado...");
+                    Console.WriteLine("------------------------------------------------------------------------");
+                    Console.WriteLine("Ahora pueder hablar con el servidor");
+                    Console.WriteLine("Nota: Cuando quieras cerrar la conexión con el servidor, escribe 'chao'");
+                    Console.WriteLine("------------------------------------------------------------------------");
+                    Console.WriteLine("");
+
+                    
+                    Console.WriteLine("Comienza a escribir al servidor");
+                    Console.Write("Tu: ");
+                    string nombre = Console.ReadLine().Trim();
+                    clienteSocket.Escribir(nombre);
+
+                    bool mantenerConexion = true;
+                    while (mantenerConexion)
+                    {
+
+                        string respuesta = clienteSocket.Leer();
+                        Console.WriteLine("");
+                        Console.WriteLine("SERVIDOR: {0}", respuesta);
+
+                        if (respuesta == "chao")
+                        {
+                            Console.WriteLine("--------------------Se cerró la conexión--------------------");
+                            clienteSocket.Escribir(respuesta);
+                            mantenerConexion = false;
+                            clienteSocket.Desconectar();
+                        }
+                        else
+                        {
+                            
+                            Console.WriteLine("");
+                            Console.Write("Tu: ");
+                            nombre = Console.ReadLine().Trim();
+                            clienteSocket.Escribir(nombre);
+                        }
+
+
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("¡¡¡Error de Comunicacion!!!. Revisa si ingresastes bien los datos");
+
+                }
+
+                bool repCiclo = true;
+
+                do
+                {
+                    Console.WriteLine("¿Volver a conectar?");
+                    Console.WriteLine("1. Sí");
+                    Console.WriteLine("0. No");
+                    Console.Write(": ");
+                    string opcion = Console.ReadLine().Trim();
+                    if (opcion == "0")
+                    {
+                        repetir = false;
+                        repCiclo = false;
+                    }
+                    else if (opcion == "1")
+                    {
+                        repCiclo = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("¡Debes seleccionar unas de las opciones!");
+                    }
+
+                } while (repCiclo);
+
+            } while (repetir);
+
+
+        }
+    }
+}
